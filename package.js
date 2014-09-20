90373002c87b562bc6ca2492fef49e94dc85b6fc
Yasin Uslu <nepjua@gmail.com>
2014-09-20 13:18:46 +0300

    version bump for publish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'A package that makes it easy to manage meta-data',
-  version: "0.1.2",
+  version: "0.1.3",
   git: "https://github.com/yasinuslu/blaze-meta.git",
   name: "yasinuslu:blaze-meta"
 });
